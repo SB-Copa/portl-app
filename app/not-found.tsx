65de48d9470830d32a8abe92ac982d577f305790
Miguel Arciaga <miguelarciagaa@gmail.com>
2025-12-16 21:50:28 +0800

    Initial commit

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center">
+      <h1>Page Not Found!</h1>
+    </div>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default async function HomePage() {
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center">
+      <h1>Homepage</h1>
+    </div>
+  );
+}
