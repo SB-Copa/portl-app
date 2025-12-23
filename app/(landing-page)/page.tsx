8cc8b463a5e7a22c70a136f5f559d72831dcdfef
Miguel Arciaga <miguelarciagaa@gmail.com>
2025-12-23 20:04:58 +0800

    fixed tailwind setup for radius

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default async function HomePage() {
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center">
-      <h1>Homepage</h1>
+      <Button>Test button</Button>
     </div>
   );
 }
